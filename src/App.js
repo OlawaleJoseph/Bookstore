4e0ff620e7d67d5cee929ebd5b383662dba55161
OlawaleJoseph <adedekojoseph@gmail.com>
2020-12-10 13:28:26 +0100

    remove report web vitals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';function App() {
+import React from 'react'
+
+const App = () => {
   return (
     <div>
-    Hello
+      Bookstore App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
