9d5ea37a9f4263be867f14fb32895137ef2adb48
OlawaleJoseph <adedekojoseph@gmail.com>
2020-12-11 14:15:36 +0100

    create category filter action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_BOOK, REMOVE_BOOK } from './type';
+import { ADD_BOOK, REMOVE_BOOK, FILTER_BOOK } from './type';
 
 export const addBook = book => ({
   type: ADD_BOOK,
@@ -8,4 +8,9 @@ export const addBook = book => ({
 export const removeBook = id => ({
   type: REMOVE_BOOK,
   payload: id,
+});
+
+export const filterBooks = category => ({
+  type: FILTER_BOOK,
+  payload: category,
 });
