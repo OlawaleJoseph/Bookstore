c79c559af14a0651f1779424b8721c168d950381
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-12-11 10:34:02 +0100

    Made the BooksForm component stateful

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addBook } from '../actions';
 import { categories, generateRandomNumber } from '../common';
 
-const BookForm = () => {
+const initialValue = {
+  title: '',
+  author: '',
+  category: '',
+}; const BookForm = ({ createBook }) => {
+  const [input, setInput] = useState(initialValue);
   const allCategories = categories.map(category => (
     <option
       key={generateRandomNumber()}
@@ -9,19 +17,48 @@ const BookForm = () => {
     >
       {category}
     </option>
-  ));
-  return (
+  )); const handleChange = e => {
+    const { value } = e.target;
+    setInput({ ...input, [e.target.name]: value.toUpperCase() });
+  }; const validateInputError = () => {
+    const { title, author, category } = input;
+    let error = '';
+    if (!title) {
+      error = 'Title is required';
+    } else if (title.length < 2) {
+      error = 'Title should have minimum of two(2) characters';
+    } else if (!author) {
+      error = 'Author is required';
+    } else if (author.length < 3) {
+      error = 'Author should have minimum of three(3) characters';
+    } else if (!category) {
+      error = 'Category is required';
+    }
+    return error;
+  }; const handleSubmit = e => {
+    e.preventDefault();
+    const error = validateInputError();
+    if (!error) {
+      createBook({ ...input, id: generateRandomNumber() });
+      setInput(initialValue);
+    } else {
+      document.querySelector('#error').textContent = error;
+    }
+  }; return (
     <div>
-      <form action="">
-        <input type="text" name="title" placeholder="Title" />
-        <input type="text" name="author" placeholder="Author" />
-        <select name="category" id="category" placeholder="Category">
+      <form onSubmit={handleSubmit}>
+        <p id="error" />
+        <input type="text" name="title" value={input.title} placeholder="Title" onChange={handleChange} />
+        <input type="text" name="author" value={input.author} placeholder="Author" onChange={handleChange} />
+        <select name="category" value={input.category} id="category" placeholder="Category" onChange={handleChange}>
           {allCategories}
         </select>
         <button type="submit">Add Book</button>
       </form>
     </div>
   );
-};
-
-export default BookForm;
+}; BookForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+}; const mapDispatchToProps = dispatch => ({
+  createBook: book => dispatch(addBook(book)),
+}); export default connect(null, mapDispatchToProps)(BookForm);
